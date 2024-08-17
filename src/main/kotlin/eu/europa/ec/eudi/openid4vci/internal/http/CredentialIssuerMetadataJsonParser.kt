b321d40259e362c34d3e8e2c10a3fd23e8c52935
Henry Jalonen <henry.jalonen@reaktor.com>
2024-08-17 23:47:49 +0300

    Add legacy cryptographic_binding_methods_supported value mso

--- src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/http/CredentialIssuerMetadataJsonParser.kt
+++ src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/http/CredentialIssuerMetadataJsonParser.kt
@@ -586,6 +586,7 @@ private fun cryptographicBindingMethodOf(s: String): CryptographicBindingMethod 
     when {
         s == "jwk" -> CryptographicBindingMethod.JWK
         s == "cose_key" -> CryptographicBindingMethod.COSE
+        s == "mso" -> CryptographicBindingMethod.COSE
         s.startsWith("did") -> CryptographicBindingMethod.DID(s)
         else -> error("Unknown Cryptographic Binding Method '$s'")
     }
